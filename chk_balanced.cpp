cb0a08bac6609582abc761e269a03bf21f26e621
SamWang8891 <73735177+SamWang8891TW@users.noreply.github.com>
2023-10-07 11:06:15 +0800

    Update chk_balanced.cpp

--- chk_balanced.cpp
+++ chk_balanced.cpp
@@ -9,7 +9,7 @@ int main(){
     std::cout << "Please enter character(s) followed by an enter: ";
     std::cin >> carray;
 
-    while(counter_loop){
+    while(counter_loop){ //先確認有幾個字
         if(carray[counter])
             counter_loop = false;
         else{
@@ -20,8 +20,8 @@ int main(){
     std::cout << "You have entered " << counter+1 << " characters." << "\n";
     std::cout << "The program needs to check its balance for " << ceil((counter+1)/2.0) << " times.\n"; //The decimal (.0) for 2.0 is VERY IMPORTANT, floating is needed in order to get the correct value.
 
-    for(int i = ceil((counter+1)/2.0); i>=0; i--){
-        while(except_cirrumstance){
+    for(int i = ceil((counter+1)/2.0); i>=0; i--){ //開始檢查對稱
+        while(except_cirrumstance){ //只有一個字
             std::cout << "Start checking...\n";
             if(counter_from_begin==counter){
                 std::cout << "Do'n mess with me!\n";
@@ -29,15 +29,15 @@ int main(){
             }
             except_cirrumstance = false;
         }
-        if(counter_from_begin >= counter){
+        if(counter_from_begin >= counter){ //如果已經檢查到底了就要跳脫，這個要擺在前面，不然會出錯
             std::cout << "The character(s) you entered are blanced!\n";
             break;
         }
-        else if(carray[counter_from_begin] == carray[counter]){
+        else if(carray[counter_from_begin] == carray[counter]){ //持續檢查
             counter_from_begin ++;
             counter --;
         }
-        else{
+        else{ //一遇到沒有對稱的就跳脫並告知使用者
             std::cout << "The character(s) you entered are NOT balanced.\n";
             break;
         }
