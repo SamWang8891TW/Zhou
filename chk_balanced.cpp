4d66650f458c097ad45e66710adc56faf6c9f09d
SamWang8891 <73735177+SamWang8891TW@users.noreply.github.com>
2023-10-07 11:01:02 +0800

    Create chk_balanced.cpp

---
+++ chk_balanced.cpp
@@ -0,0 +1,49 @@
+#include <complex.h>
+#include <iostream>
+#include <string>
+
+int main(){
+    char carray[101];
+    int counter = 100, counter_from_begin = 0, temp;
+    bool counter_loop = true, except_cirrumstance = true;
+    std::cout << "Please enter character(s) followed by an enter: ";
+    std::cin >> carray;
+
+    while(counter_loop){
+        if(carray[counter])
+            counter_loop = false;
+        else{
+            counter --;
+        }
+    }
+
+    std::cout << "You have entered " << counter+1 << " characters." << "\n";
+    std::cout << "The program needs to check its balance for " << ceil((counter+1)/2.0) << " times.\n"; //The decimal (.0) for 2.0 is VERY IMPORTANT, floating is needed in order to get the correct value.
+    temp = ceil((counter+1)/2.0);
+    std::cout << "temp val: " << temp << "\n";
+
+    for(int i = temp; i>=0; i--){
+        while(except_cirrumstance){
+            std::cout << "Start checking...\n";
+            if(counter_from_begin==counter){
+                std::cout << "Do'n mess with me!\n";
+                goto endLoop;
+            }
+            except_cirrumstance = false;
+        }
+        if(counter_from_begin >= counter){
+            std::cout << "The character(s) you entered are blanced!\n";
+            break;
+        }
+        else if(carray[counter_from_begin] == carray[counter]){
+            counter_from_begin ++;
+            counter --;
+        }
+        else{
+            std::cout << "The character(s) you entered are NOT balanced.\n";
+            break;
+        }
+    }
+    endLoop:
+    return 0;
+}
